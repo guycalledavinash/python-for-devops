70d242d62668a356cc29432c8867895864df986d
Avinash <90386560+guycalledavinash@users.noreply.github.com>
2025-02-22 18:15:43 -0500

    Create excel-cond-fort.py

---
+++ Day-07/excel-cond-fort.py
@@ -0,0 +1,20 @@
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+from random import randint
+
+wb = Workbook()
+ws = wb.active
+
+for row in range(2, 12):
+    ws.append([randint(10, 100)])
+
+for row in ws.iter_rows(min_row=2, max_row=11, min_col=1, max_col=1):
+    for cell in row:
+        if cell.value > 80:
+            cell.fill = PatternFill(start_color='FF9999', end_color='FF9999', fill_type='solid')
+        elif cell.value > 50:
+            cell.fill = PatternFill(start_color='FFFF99', end_color='FFFF99', fill_type='solid')
+        else:
+            cell.fill = PatternFill(start_color='99FF99', end_color='99FF99', fill_type='solid')
+
+wb.save("formatted.xlsx")
